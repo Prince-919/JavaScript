7de17dace4fbf795823f212a83de9dec16f6bc63
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-23 14:33:56 -0700

    1st Interview Question

---
+++ diff_null_vs_undefined.js
@@ -0,0 +1,13 @@
+
+/************ Interview Question 1 **************/
+/****Difference between null vs undefined ************/ 
+
+// Null
+var iAmUseLess = null;
+console.log(iAmUseLess);
+console.log(typeof(iAmUseLess));   // 2nd bug in javaScript
+
+// Undefined
+var iAmStandBy;
+console.log(iAmStandBy);
+console.log(typeof(iAmStandBy));
