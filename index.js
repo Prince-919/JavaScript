c8a6a1ef892656d21531bab1c93520634bf288ab
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-23 14:05:16 -0700

    Create data Type

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ var myName = 'Prince Sharma';
 
 /**** Section 1 DataTypes in javaScript */
 
+//String 
 var myName = "Prince Sharma";
 console.log(myName);
+
+// types of operator
+console.log(typeof(myName)); 
+
+
+// Number
+var myAge = 21;
+console.log(typeof(myAge));
+
+// undefined  // boolean
+var iAmPrince = true;
+console.log(iAmPrince);
+console.log(typeof(iAmPrince));
+
