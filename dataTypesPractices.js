342a4b359ca031d2865d2dd2426a447c344a84cc
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-23 14:20:43 -0700

    Create DataTypes Practices

---
+++ dataTypesPractices.js
@@ -0,0 +1,20 @@
+
+/***************************Data Type Practices ***************************/
+
+console.log(10 + "20");
+console.log(9 - "5");  // it is bug
+
+console.log("java" + "Script");
+
+// two empty
+console.log(" " + " ");
+console.log(" " + 0);
+
+// String + String (NaN ---> Not a Number)
+console.log("Prince" - "Sharma");
+
+// boolean
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
