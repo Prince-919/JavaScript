d6b5fca5aa03b3d8c937b6d99e5595b9994ed495
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-23 20:41:24 -0700

    NaN Question Practices

---
+++ dataTypePractice2.js
@@ -0,0 +1,8 @@
+
+/*********** NaN Practice  *******************/
+
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN(NaN));
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
