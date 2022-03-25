c599d0d82a91b36362677ec6f30005c9d8687f09
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-25 11:32:26 -0700

    Create comparison Operator

---
+++ PS8_comperison_Operator.js
@@ -0,0 +1,12 @@
+/**************** Comparison operator **************/
+// A Comparison Operator compare based on whether the comparison is true
+// return a logical value based on whether the comparison is true
+
+var a = 30;
+var b = 10;
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
