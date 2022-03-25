6b74b136a2929e55d5f5130a3914d1132d328060
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-24 19:46:05 -0700

    Create Arithmetic Operator

--- PS6_arithmetic_Operator.js
+++ PS6_arithmetic_Operator.js
@@ -2,3 +2,10 @@
 // An arithmetic operator takes numerical values
 // (either literals or variables ) as their operator.
 //  and returns a single numerical value
+
+console.log(3 + 3);
+console.log(10 - 5);
+console.log(20 / 5);
+console.log(5 * 6);
+
+console.log("Remainder Operator : " + 27 % 4);
