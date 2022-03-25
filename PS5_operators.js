2919fd62509bc780ee2c2cacb70c0ca5fe3a419c
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-24 19:34:03 -0700

    Create operator

---
+++ PS5_operators.js
@@ -0,0 +1,9 @@
+
+/*********** Assignment Operator ********************/
+// An assignment operator assigns a value to left operand
+// based on the value of its right operand.
+// The simple assignment operator is equal ( = )
+
+var x = 5;
+var y = 5;
+console.log("is both the x and y are equal or not" + x == y);
