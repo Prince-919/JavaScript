af9927cae97c2115e8173cad8b10666d10e7d20d
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-24 19:41:41 -0700

    define Arithmetic Operator

---
+++ PS6_arithmetic_Operator.js
@@ -0,0 +1,4 @@
+/************* Arithmetic Operators ***************/
+// An arithmetic operator takes numerical values
+// (either literals or variables ) as their operator.
+//  and returns a single numerical value
