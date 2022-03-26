59ea8acee1073ff71c5c272d08128324cd42f966
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-25 19:24:49 -0700

    Interview Question 4

---
+++ PS12_interviewQuestion4.js
@@ -0,0 +1,23 @@
+
+/********🙋🙋 Interview Question 4 🙋‍♂️🙋‍♂️ ********/
+// What is the Difference between == vs ===
+
+var a = 5;
+var b = '5';
+
+// ==
+console.log(" == ");
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(b);
+console.log(a == b);
+
+// ===
+var x = 2;
+var y = '2';
+console.log(" === ");
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(y);
+console.log(x === y);
+
