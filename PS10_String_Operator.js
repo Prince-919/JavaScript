5fdb19090ef6214f5dc66ddc131e5a2ef79df537
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-25 18:43:57 -0700

    Create String Operator Concatenation (+)

---
+++ PS10_String_Operator.js
@@ -0,0 +1,11 @@
+/*********** String Concatenation (Operator)*******/
+/**  The Concatenation operator (+) concatenate two string values together, returning another string that is the union of the two oprand string **********/
+
+console.log("Hello World!");
+console.log("Hello " + "World!");
+
+var myName = "Prince";
+console.log(myName + " Sharma");
+console.log(myName + " Kumar");
+console.log(myName + " Kumar Sharma");
+
