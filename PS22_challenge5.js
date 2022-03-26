ddecc63fa1195b0f66f14db435955a90f1075d8a
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 13:45:16 -0800

    Challenge 6

---
+++ PS22_challenge5.js
@@ -0,0 +1,10 @@
+
+/******* Challenge 6️⃣ *******/
+// JavaScript program to print table for given number(8,9,12,15) using for loop?
+
+for(var i=1; i<=10; i++)
+{
+    debugger;
+    var tableOf = 8;
+    console.log(tableOf + "  * " + i + " = " + tableOf * i);
+} 
