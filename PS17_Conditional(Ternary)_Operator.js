2efc5ae3b240f5843eace763ca2acb0c296f34fb
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 12:26:09 -0800

    Create Conditional Operator

---
+++ PS17_Conditional(Ternary)_Operator.js
@@ -0,0 +1,20 @@
+
+/********* 2️⃣Conditional (ternary) Operator ******/
+// The conditional (ternary ) operator is the only JavaScript Operator that takes three operands
+
+// Syntax 
+// variableName = (condition) ? value1 : value2
+
+// method if else
+var age = 19;
+if(age >= 18)
+{
+    console.log("you can vate");
+}else{
+    console.log("you con't vote");
+}
+
+// method ternary 
+
+var age =19;
+console.log((age = 18) ? "you can vote" : "you con't vote");
