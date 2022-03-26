f7da3c78f65732ab9e9cf0dbb5e115d92f044d07
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 13:03:10 -0800

    Create While Loop Statement

---
+++ PS19_WhileLoopStatement.js
@@ -0,0 +1,12 @@
+
+/********4️⃣ While loop statement **********/
+// The while statement creates a loop that executes a specified statement as long as the best condition evaluates to true
+
+
+var num = 1;
+// block scope
+while(num <= 20)
+{
+    console.log(num);  // infinte loop
+    num++;
+}
