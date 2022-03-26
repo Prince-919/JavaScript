3a68c3be92d12fc3c97aa13f798fb9127fa4b9a6
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-25 19:12:29 -0700

    Challenge 4

---
+++ PS11_Challenge_Time.js
@@ -0,0 +1,38 @@
+/*********** Challenge Time  **********************/
+// what will be the output of 3**3
+// what will be the output, when we add a number and a string 
+// write a program to swap two number
+// write a program to swap two numbers without using third variable? 
+
+
+
+// solution 1.✓ 
+console.log("Soltion 1 : ");
+console.log(3**3);
+
+// Soltion 2. ✓
+console.log("Solution 2 : ");
+console.log(10 + " Sharma");
+
+// Solution 3. ✓
+console.log("Solution 3 : ");
+
+var a = 10;
+var b = 20;
+var c = b;
+    b = a;
+    a = c;
+console.log("The value a is : " + a);    
+console.log("The value a is : " + b);  
+
+// Solution 4.✓
+console.log("Solution 4 : ");
+var x = 5;
+var y = 15;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log(x);
+console.log(y);
+
+
