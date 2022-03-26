79dbe393451f7b52c6a96566a9ad8c1a604d61d9
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 08:26:01 -0800

    Create IF else loop

---
+++ PS13_IF_else.js
@@ -0,0 +1,11 @@
+/************ 1️⃣If....Else ************/
+// The if statement excuted a statement if a specified condition is truthy
+// If the condition is falsy, another statement can be executed.
+
+
+var tomorrow = "rain";
+if(tomorrow == "rain"){
+    console.log("take a raincoat");
+}else{
+    console.log("No Need to take a raincoat");
+}
