34a6d92542584f7e05c57b46bac8683732f57f71
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 13:30:50 -0800

    Create For Loop

---
+++ PS21_ForLoop.js
@@ -0,0 +1,16 @@
+
+
+/********* 6️⃣ For Loop Statement ***********/
+
+// Syntax
+
+// for(intializer; condition; iteration)
+//{
+    // Code to be executed
+//}
+
+for(var i=1; i<=10; i++)
+{
+    debugger;
+    console.log(i);
+} 
