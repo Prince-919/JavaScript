99507dd35b1110eecfcdbffaa5322d9fbb8fe2a9
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 13:18:04 -0800

    Create Do while loop statement

---
+++ PS20_Do_WhileLoop.js
@@ -0,0 +1,22 @@
+
+/********* do While Loop Statement *********/
+// Difference while loop VS Do while loop
+
+// while loop
+console.log("While Loop Statement");
+var a = 1;
+while(a <= 10)
+{
+    console.log(a);
+    a++;
+}
+
+console.log("Do While Loop Statement");
+
+// do while loop
+
+var i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i <= 20);
