c7e568da453a0b6a78b28ff3e0e99ee20f73ea23
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-26 11:40:33 -0800

    Total 5 False value in JS otherwise All True

---
+++ PS16_truthy_Vs_falsy.js
@@ -0,0 +1,11 @@
+// what is truthy and falsy value in javascript
+// we have total 5 falsy value in javascript
+// 0, "", undefined, null, NaN, false** is false antway
+
+
+if(score = 0)
+{
+    console.log("OMG, we loss the game 😲");
+}else{
+    console.log("Yay, we Won the game 😊");
+}
