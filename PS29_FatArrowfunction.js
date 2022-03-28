5060a01c63563d71553dc6a084cdae7e5b71cfb4
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-27 19:35:35 -0800

    Create Fat Arrow function

---
+++ PS29_FatArrowfunction.js
@@ -0,0 +1,8 @@
+// const sum = () => {
+//     // let a = 5; let b = 6;
+//     return `The sum of the two number is : ${(a=5)+(b=5)}`;
+// }
+// console.log(sum());
+
+const sum = () => `the sum of the two number is : ${(a=5)+(b=5)}`;
+console.log(sum());
