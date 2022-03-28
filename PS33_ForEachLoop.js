48eb17844161e5c7c10e679b69d2a3d3956007ff
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-28 02:05:32 -0800

    Create For Each Loop

---
+++ PS33_ForEachLoop.js
@@ -0,0 +1,7 @@
+// For Each Loop
+
+var myFriends = ['prince','rahul','karan','ram'];
+myFriends.forEach(function(element, index, array)
+{
+    console.log(element + " index : " + index + array);
+});
