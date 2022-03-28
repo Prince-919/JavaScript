b8b573bddd5d263e5f9df8f649efce6be7cf2267
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-28 01:11:39 -0800

    Create for In Loop

---
+++ PS31_For_IN_loop.js
@@ -0,0 +1,11 @@
+// var myFriends = ['prince','rahul','karan','ram'];
+// for(var i=0; i<myFriends.length; i++)
+// {
+//     console.log(myFriends[i]);
+// }
+
+var myFriends = ['prince','rahul','karan','ram'];
+for(let element in myFriends)
+{
+    console.log(element);
+}
