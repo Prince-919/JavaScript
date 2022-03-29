8059380ff64f48d9e0623185ec1cffb858164e5d
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-29 15:28:44 -0800

    Create Escape Character

---
+++ PS42_escapeCharacter.js
@@ -0,0 +1,8 @@
+// Escape Character
+
+let anySentence = "We are the so-called \"Vikings\" from the north.";
+console.log(anySentence);
+
+//if you don't want to mess , simply use the alternate qaotes
+let anySentence2 ='We are the so-called "Vikings" from the north.';
+console.log(anySentence2);
