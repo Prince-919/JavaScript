5a03f6d43491080355df6ee4223d95891067c159
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-29 02:31:33 -0800

    Create pop and shift

---
+++ PS35_pop.js
@@ -0,0 +1,15 @@
+
+//pop method (delete last index)
+
+// const plants = ['broccoli','cauliflower','kale', 'tomato', 'cabbage'];
+// console.log(plants);
+// console.log(plants.pop());
+// console.log(plants);
+
+
+// shift delete first index
+
+const plants = ['broccoli','cauliflower','kale', 'tomato', 'cabbage'];
+console.log(plants);
+console.log(plants.shift());
+console.log(plants);
