ca713cf3e2d84713813d7a54f907f0fa14b54055
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-29 15:42:15 -0800

    Create String in a String

---
+++ PS43_StringToString.js
@@ -0,0 +1,12 @@
+
+// Finding a String in a String
+
+const myBiaData = 'I am Full Stack Developer';
+console.log(myBiaData.indexOf("Stack"));
+// console.log(myBiaData.indexOf("developer"));
+// console.log(myBiaData.indexOf("Prince"));
+console.log(myBiaData.indexOf("F"));
+
+console.log("Last Index : ");
+const myBiaData2 = 'I am Full Stack Developer';
+console.log(myBiaData2.indexOf("Stack", 6));
