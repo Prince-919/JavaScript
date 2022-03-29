0e9885090fd01cda5c275019b35fb7805f1270a5
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-29 02:22:33 -0800

    Create First Index and Last Index

---
+++ PS34_SearchingArray.js
@@ -0,0 +1,23 @@
+// Array Subsection Perform CRUD
+
+// const  animals = ['pis','goats','sheep'];
+// const count = animals.push('chicken');
+
+// push (add (last index))
+// animals.push('chicken');
+// console.log(animals);
+// console.log("Array Of Lenght : "+count);
+
+//Example 1
+// const  animals = ['pis','goats','sheep'];
+
+// // adding (starting address)
+// // animals.unshift('chicken');
+// animals.unshift('chicken','cats', 'cow');
+
+// console.log(animals);
+
+//Example 2
+const myNumber = [1,2,3,5];
+myNumber.unshift(4,6);
+console.log(myNumber);
