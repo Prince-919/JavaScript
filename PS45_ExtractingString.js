cc402aab461da8d43761b891211f624d781dae42
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-30 07:24:21 -0800

    Create Extracting String

---
+++ PS45_ExtractingString.js
@@ -0,0 +1,11 @@
+
+// Extracting String Parts
+
+let str = "Apple, Banana, Kiwi, Mango";
+
+let res = str.slice(0,5);
+let res2 = str.slice(7,-2);
+let res3 = str.slice(7,-2);
+console.log(res);
+console.log(res2);
+console.log(res3);
