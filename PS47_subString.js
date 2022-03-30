6d17968518aae999bc747a836273698ab8d79e38
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-30 11:40:20 -0800

    Create subString

---
+++ PS47_subString.js
@@ -0,0 +1,12 @@
+
+// SubString () Method
+// SubString () is similer to clice()
+// The difference is that subString () connot accept
+
+// let str = "Apple, Banana, Kiwi";
+// let res = str.substring(0,4);
+// let res2 = str.substring(8,-2);
+// console.log(res);
+// console.log(res2);
+
+let str = "Apple, Banana, Kiwi"; 
